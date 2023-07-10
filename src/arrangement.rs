44e2b2d35b4d215a6b014bcfd475ca9da30eccbc
Noah <nbaculi@hotmail.com>
2023-07-09 22:34:03 -0700

    no_duplicate_strings

--- src/arrangement.rs
+++ src/arrangement.rs
@@ -16,6 +16,16 @@ pub struct InvalidInput {
 //     non_zero_avg_fret: f32,
 // }
 
+// TODO! re-examine namespace of functions (object functions vs standalone) (public vs private)
+// TODO! let non_zero_fret_avg = non_zero_frets.iter().sum::<usize>() as f32 /
+// non_zero_frets.len() as f32;
+// TODO! filter unplayable fingering options from beat_fingering_candidates (based
+// on the fret width and whether there are any candidates with smaller fret
+// widths)
+// TODO! pathfinding (https://docs.rs/pathfinding/latest/pathfinding/)
+// TODO! investigate property testing (https://altsysrq.github.io/proptest-book/)
+// TODO! benchmarking via Criterion (https://crates.io/crates/criterion)
+
 pub type PitchVec<T> = Vec<T>;
 type BeatVec<T> = Vec<T>;
 
@@ -24,107 +34,88 @@ pub struct Arrangement {}
 
 impl Arrangement {
     pub fn new(guitar: Guitar, input_pitches: Vec<BeatVec<Pitch>>) -> Result<Self> {
-        // TODO! add type alias for BeatVec, PitchVec, Candidates, ...
-        // https://doc.rust-lang.org/book/ch19-04-advanced-types.html#creating-type-synonyms-with-type-aliases
-
-        let pitch_fingering_options: Vec<BeatVec<PitchVec<Fingering>>> =
-            Arrangement::validate_fingerings(&guitar, &input_pitches)?;
+        let pitch_fingering_candidates: Vec<BeatVec<PitchVec<Fingering>>> =
+            validate_fingerings(&guitar, &input_pitches)?;
 
         // let x: Vec<_> = vec![vec![1, 2], vec![10, 20], vec![100, 200]]
         //     .into_iter()
         //     .multi_cartesian_product()
         //     .collect();
         // dbg!(&x);
-        // dbg!(&pitch_fingering_options);
-        let beat_fingering_options = pitch_fingering_options
+        // dbg!(&pitch_fingering_candidates);
+        let beat_fingering_candidates = pitch_fingering_candidates
             .iter()
-            .map(|beat_pitch_options| {
-                beat_pitch_options
+            .map(|beat_pitch_candidates| {
+                beat_pitch_candidates
                     .iter()
                     .multi_cartesian_product()
-                    .filter(|beat_fingering_option| {
-                        Arrangement::no_duplicate_strings(beat_fingering_option)
-                    })
+                    .filter(no_duplicate_strings)
                     .collect::<Vec<_>>()
             })
             .collect::<Vec<_>>();
-        dbg!(&beat_fingering_options);
+        dbg!(&beat_fingering_candidates);
 
         Ok(Arrangement {})
     }
+}
 
-    /// Generates fingerings for each pitch, and returns a result containing the fingerings or
-    /// an error message if any impossible pitches (with no fingerings) are found.
-    ///
-    /// Arguments:
-    ///
-    /// * `guitar`: A reference to a `Guitar` object, which contains information about the guitar's
-    /// string ranges.
-    /// * `input_pitches`: A slice of vectors, where each vector represents a beat and contains a
-    /// vector of pitches.
-    ///
-    /// Returns:
-    ///
-    /// The function `validate_fingerings` returns a `Result` containing either a
-    /// `Vec<Vec<Vec<Fingering>>>` if the input pitches are valid, or an `Err` containing an error
-    /// message if there are invalid pitches.
-    fn validate_fingerings(
-        guitar: &Guitar,
-        input_pitches: &[BeatVec<Pitch>],
-    ) -> Result<Vec<BeatVec<PitchVec<Fingering>>>> {
-        let mut impossible_pitches: Vec<InvalidInput> = vec![];
-        let fingerings: Vec<BeatVec<PitchVec<Fingering>>> = input_pitches[0..]
-            .iter()
-            .enumerate()
-            .map(|(beat_index, beat_pitches)| {
-                beat_pitches
-                    .iter()
-                    .map(|beat_pitch| {
-                        let pitch_fingerings: PitchVec<Fingering> =
-                            Guitar::generate_pitch_fingerings(&guitar.string_ranges, beat_pitch);
-                        if pitch_fingerings.is_empty() {
-                            impossible_pitches.push(InvalidInput {
-                                value: format!("{:?}", beat_pitch),
-                                line_number: (beat_index as u16) + 1,
-                            })
-                        }
-                        pitch_fingerings
-                    })
-                    .collect()
-            })
-            .collect();
-
-        if !impossible_pitches.is_empty() {
-            let error_string = impossible_pitches
+/// Generates fingerings for each pitch, and returns a result containing the fingerings or
+/// an error message if any impossible pitches (with no fingerings) are found.
+///
+/// Arguments:
+///
+/// * `guitar`: A reference to a `Guitar` object, which contains information about the guitar's
+/// string ranges.
+/// * `input_pitches`: A slice of vectors, where each vector represents a beat and contains a
+/// vector of pitches.
+///
+/// Returns:
+///
+/// The function `validate_fingerings` returns a `Result` containing either a
+/// `Vec<Vec<Vec<Fingering>>>` if the input pitches are valid, or an `Err` containing an error
+/// message if there are invalid pitches.
+fn validate_fingerings(
+    guitar: &Guitar,
+    input_pitches: &[BeatVec<Pitch>],
+) -> Result<Vec<BeatVec<PitchVec<Fingering>>>> {
+    let mut impossible_pitches: Vec<InvalidInput> = vec![];
+    let fingerings: Vec<BeatVec<PitchVec<Fingering>>> = input_pitches[0..]
+        .iter()
+        .enumerate()
+        .map(|(beat_index, beat_pitches)| {
+            beat_pitches
                 .iter()
-                .map(|invalid_input| {
-                    format!(
-                        "Pitch {} on line {} cannot be played on any strings of the configured guitar.",
-                        invalid_input.value, invalid_input.line_number
-                    )
+                .map(|beat_pitch| {
+                    let pitch_fingerings: PitchVec<Fingering> =
+                        Guitar::generate_pitch_fingerings(&guitar.string_ranges, beat_pitch);
+                    if pitch_fingerings.is_empty() {
+                        impossible_pitches.push(InvalidInput {
+                            value: format!("{:?}", beat_pitch),
+                            line_number: (beat_index as u16) + 1,
+                        })
+                    }
+                    pitch_fingerings
                 })
-                .collect::<Vec<String>>()
-                .join("\n");
+                .collect()
+        })
+        .collect();
 
-            return Err(anyhow!(error_string));
-        }
-
-        Ok(fingerings)
-    }
-
-    /// Checks if there are any duplicate strings in a vector of `Fingering`
-    /// objects to ensure that all pitches can be played.
-    /// TODO! write tests
-    fn no_duplicate_strings(beat_fingering_option: &Vec<&Fingering>) -> bool {
-        let num_pitches = beat_fingering_option.len();
-        let num_strings = beat_fingering_option
+    if !impossible_pitches.is_empty() {
+        let error_string = impossible_pitches
             .iter()
-            .map(|fingering| fingering.string_number)
-            .collect::<HashSet<_>>()
-            .len();
+            .map(|invalid_input| {
+                format!(
+                    "Pitch {} on line {} cannot be played on any strings of the configured guitar.",
+                    invalid_input.value, invalid_input.line_number
+                )
+            })
+            .collect::<Vec<String>>()
+            .join("\n");
 
-        num_pitches == num_strings
+        return Err(anyhow!(error_string));
     }
+
+    Ok(fingerings)
 }
 
 #[cfg(test)]
@@ -209,7 +200,7 @@ mod test_validate_fingerings {
         )]];
 
         assert_eq!(
-            Arrangement::validate_fingerings(&guitar, &input_pitches).unwrap(),
+            validate_fingerings(&guitar, &input_pitches).unwrap(),
             expected_fingerings
         );
     }
@@ -233,7 +224,7 @@ mod test_validate_fingerings {
         ];
 
         assert_eq!(
-            Arrangement::validate_fingerings(&guitar, &input_pitches).unwrap(),
+            validate_fingerings(&guitar, &input_pitches).unwrap(),
             expected_fingerings
         );
     }
@@ -242,7 +233,7 @@ mod test_validate_fingerings {
         let guitar = generate_standard_guitar();
         let input_pitches = vec![vec![Pitch::B9]];
 
-        let error = Arrangement::validate_fingerings(&guitar, &input_pitches).unwrap_err();
+        let error = validate_fingerings(&guitar, &input_pitches).unwrap_err();
         let error_string = format!("{error}");
         let expected_error_string =
             "Pitch B9 on line 1 cannot be played on any strings of the configured guitar.";
@@ -260,7 +251,7 @@ mod test_validate_fingerings {
             vec![Pitch::D4, Pitch::G4],
         ];
 
-        let error = Arrangement::validate_fingerings(&guitar, &input_pitches).unwrap_err();
+        let error = validate_fingerings(&guitar, &input_pitches).unwrap_err();
         let error_string = format!("{error}");
         let expected_error_string =
             "Pitch A1 on line 1 cannot be played on any strings of the configured guitar.\n\
@@ -268,5 +259,106 @@ mod test_validate_fingerings {
             Pitch B1 on line 4 cannot be played on any strings of the configured guitar.\n\
             Pitch D2 on line 5 cannot be played on any strings of the configured guitar.";
         assert_eq!(error_string, expected_error_string);
+    }
+}
+
+/// Checks if there are any duplicate strings in a vector of `Fingering`
+/// objects to ensure that all pitches can be played.
+fn no_duplicate_strings(beat_fingering_option: &Vec<&Fingering>) -> bool {
+    let num_pitches = beat_fingering_option.len();
+    let num_strings = beat_fingering_option
+        .iter()
+        .map(|fingering| fingering.string_number)
+        .collect::<HashSet<_>>()
+        .len();
+
+    num_pitches == num_strings
+}
+
+#[cfg(test)]
+mod test_no_duplicate_strings {
+    use super::*;
+    use crate::StringNumber;
+
+    #[test]
+    fn valid_simple() {
+        let fingering_1 = Fingering {
+            pitch: Pitch::B6,
+            string_number: StringNumber::new(2).unwrap(),
+            fret: 3,
+        };
+        let beat_fingering_option: &Vec<&Fingering> = &vec![&fingering_1];
+
+        assert!(no_duplicate_strings(beat_fingering_option));
+    }
+    #[test]
+    fn valid_complex() {
+        let fingering_1 = Fingering {
+            pitch: Pitch::CSharp2,
+            string_number: StringNumber::new(1).unwrap(),
+            fret: 1,
+        };
+        let fingering_2 = Fingering {
+            pitch: Pitch::F4,
+            string_number: StringNumber::new(2).unwrap(),
+            fret: 3,
+        };
+        let fingering_3 = Fingering {
+            pitch: Pitch::A5,
+            string_number: StringNumber::new(4).unwrap(),
+            fret: 4,
+        };
+        let fingering_4 = Fingering {
+            pitch: Pitch::DSharp6,
+            string_number: StringNumber::new(11).unwrap(),
+            fret: 0,
+        };
+        let beat_fingering_option: &Vec<&Fingering> =
+            &vec![&fingering_1, &fingering_2, &fingering_3, &fingering_4];
+
+        assert!(no_duplicate_strings(beat_fingering_option));
+    }
+    #[test]
+    fn invalid_simple() {
+        let fingering_1 = Fingering {
+            pitch: Pitch::CSharp2,
+            string_number: StringNumber::new(4).unwrap(),
+            fret: 1,
+        };
+        let fingering_2 = Fingering {
+            pitch: Pitch::F4,
+            string_number: StringNumber::new(4).unwrap(),
+            fret: 3,
+        };
+        let beat_fingering_option: &Vec<&Fingering> = &vec![&fingering_1, &fingering_2];
+
+        assert!(!no_duplicate_strings(beat_fingering_option));
+    }
+    #[test]
+    fn invalid_complex() {
+        let fingering_1 = Fingering {
+            pitch: Pitch::CSharp2,
+            string_number: StringNumber::new(1).unwrap(),
+            fret: 1,
+        };
+        let fingering_2 = Fingering {
+            pitch: Pitch::F4,
+            string_number: StringNumber::new(3).unwrap(),
+            fret: 3,
+        };
+        let fingering_3 = Fingering {
+            pitch: Pitch::A5,
+            string_number: StringNumber::new(6).unwrap(),
+            fret: 4,
+        };
+        let fingering_4 = Fingering {
+            pitch: Pitch::DSharp6,
+            string_number: StringNumber::new(3).unwrap(),
+            fret: 0,
+        };
+        let beat_fingering_option: &Vec<&Fingering> =
+            &vec![&fingering_1, &fingering_2, &fingering_3, &fingering_4];
+
+        assert!(!no_duplicate_strings(beat_fingering_option));
     }
 }
